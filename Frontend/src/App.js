6ce48200bdfb0eb5d768720350f2153e5db71313
Aditi Jain <139758331+aditijain8@users.noreply.github.com>
2026-02-10 16:35:27 +0530

    Create App.js

---
+++ Frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Feed from './Feed';
+import Upload from './Upload';
+
+function App() {
+  return (
+    <div>
+      <h1>Mini Instagram Clone</h1>
+      <Upload />
+      <Feed />
+    </div>
+  );
+}
+
+export default App;
